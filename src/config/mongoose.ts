937bcac4977f477821acc14ba6c6c65d0ee40437
Andres Leon <AndresLL@digitalware.com.co>
2020-11-12 19:32:59 -0500

    sdf

--- src/config/mongoose.ts
+++ src/config/mongoose.ts
@@ -1,7 +1,9 @@
 import mongoose from 'mongoose';
 
 
-const mongo_uri = 'mongodb://localhost/dbetitc';
+//const mongo_uri = 'mongodb://localhost/dbetitc';
+const mongo_uri = 'mongodb+srv://1Sistemas:<1Sistemas>@cluster0.k3y8i.mongodb.net/<1Sistemas>?retryWrites=true&w=majority'
+
         mongoose.connect(mongo_uri,{
             useNewUrlParser: true,
             useCreateIndex: true,
